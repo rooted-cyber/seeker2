c4c7ff2b638296bce6d9948b1ee3b1707e0d152c
rooted-cyber <50019150+rooted-cyber@users.noreply.github.com>
2019-09-23 09:34:49 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,30 @@
+echo -e "\033[92m please wait....."
+sleep 2
+apt update
+apt upgrade
+apt install toilet
+apt install python
+apt install python2
+apt install curl
+apt install wget
+apt install php
+cd $HOME/seeker2
+c="toilet -f term -F gay"
+chmod 777 *
+unzip seeker.zip
+bash termux_install.sh
+bash install.sh
+cp -f seeker $PREFIX/bin
+chmod 777 $PREFIX/bin/seeker
+cd $HOME
+mv seeker2 .seeker
+clear
+echo "Successfully installed"|$c
+echo
+sleep 2
+echo -e "\033[93m Now you can start seeker type this command:-"
+echo "		seeker"|$c
+echo
+echo -e "\033[92m Press enter for start seeker"
+cd $HOME/.seeker
+python seeker.py
